7103868c19b7eb57c242ac4dd9f3f86453c4f7d2
austinollis <austin.ollis@gmail.com>
2020-09-22 22:52:33 +0000

    just startin

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import flask
+
+app = flask(__name__)
+
+@app.route('/')
+def hello():
+    returns "Hello World... again"
+
+if __name__ --"__main__":
+    app.run(host=os.environ.get('IP'),
+        port=int(os.environ.get('PORT')),
+        debug=True)
